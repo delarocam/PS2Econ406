fb28338e180564b39619b6a30486ae85bdb22880
Matthew De La Roca <mwdelaroca@gmail.com>
2021-10-16 14:11:34 -0700

    solved problem with input function not accepting list

--- demand.py
+++ demand.py
@@ -1,4 +1,5 @@
-"""This file is for building functions to track elasticity , and demand"""
+"""This file is for building functions to track elasticity,
+   and quanity demanded"""
 import math
 
 # a.) compute elasticity function
@@ -10,6 +11,8 @@ def compute_elasticity(price1: float, price2: float, quantity1: float,
     elasticity = math.log(quantity1 / quantity2) / math.log(price1 / price2)
     return elasticity
 
+# test, uncomment to run function
+# compute_elasticity(10, 12, 10, 8)
 
 # b.) elasticity classifier function
 
@@ -25,7 +28,10 @@ def check_elasticity(e: float) -> str:
         category = "inelastic"
     return category
 
-
+# test, uncomment to run function
+# check_elasticity(1.2)
+# check_elasticity(0.5)
+# check_elasticity(1)
 # c.) demand function(solved as q2 = q1 * (p2/p1)^e
 
 
@@ -35,10 +41,14 @@ def compute_demand(sales: float, initprice: float, newprice: float,
     newdemand = sales * (initprice / newprice) ** e
     return newdemand
 
-
+# test, uncomment to run function
+# compute_demand(10, 10, 12, 1.2)
 # d.) combining elasticity and Demand into main function using input function
 
+
 def main():
+    """This function is a universal function that will either compute
+    Elasticity or Demand depending on user inputs"""
     choice = input("Would you like to Calculate Elasticity or Demand?")
     if choice == "Elasticity":
         oldprice = float(input("Old price?"))
@@ -53,15 +63,22 @@ def main():
         sales = float(input("Sales?"))
         initprice = float(input("Initial price?"))
         newelasticity = float(input("Elasticity?"))
-        newrange = list(input("Additional price points?(input list)"))
+        newrange = input("Additional price points?" +
+                         "(input list of numbers seperated by a space)")
+        newrange = newrange.split()
+        print(newrange)
         demand_dict = {initprice: sales}
         for i in newrange:
-            demand_dict[float(i)] = compute_demand(sales, initprice,
-                                                   newelasticity, float(i))
+            """This forloop iterates each element of a price list
+            and computes quanity demanded in a dictionary for each"""
+            i = float(i)
+            demand_dict[i] = compute_demand(sales, initprice,
+                                            i, newelasticity)
         output = demand_dict
     else:
         output = "not valid input"
-    return output
+    return print(output)
 
 
-main()
+# test, uncomment to run function
+# main()
